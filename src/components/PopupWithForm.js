e64b7ba7c5f31e650790178c0c77144818663dae
afreakanist <afreakanist@gmail.com>
2021-05-19 01:52:14 +0600

    fix: overlay and esc click handling postponed

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,23 +1,28 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function PopupWithForm(props) {
-  useEffect(() => {
+  /* useEffect(() => {
     const handleEscClick = (event) => {
       if (event.key === "Escape") props.onClose();
     };
+    const handleOverlayClick = (event) => {
+      if (event.target.classList.contains('popup_opened')) props.onClose();
+    };
+
     document.addEventListener("keydown", handleEscClick);
+    document.querySelector(`.popup_${props.name}`).addEventListener("click", handleOverlayClick);
 
     return () => {
       document.removeEventListener("keydown", handleEscClick);
+      document.querySelector(`.popup_${props.name}`).removeEventListener("click", handleOverlayClick);
     };
-  });
+  }); */
 
   return (
     <div
       className={`popup popup_${props.name} ${
         props.isOpen ? "popup_opened" : null
       }`}
-      onClick={props.onClose}
     >
       <div className="popup__container">
         <button
